0cab5e1342b7ec86f39e8b6392d42c6827564bea
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-05-16 13:07:41 +0100

    refactor: Update main.dart to use lazy loading for images

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  ));
-}
+void main() => runApp(MaterialApp(
+      home: SimoCard(),
+    ));
 
-class Home extends StatelessWidget {
+class SimoCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: const Text(
-          'My first app',
+        title: Text(
+          'Simo Card',
           style: TextStyle(
             color: Colors.white,
-            fontSize: 40.0,
+            fontSize: 20.0,
             fontWeight: FontWeight.bold,
           ),
         ),
         centerTitle: true,
-        backgroundColor: Colors.blue,
+        backgroundColor: Color.fromARGB(255, 98, 91, 91),
+        elevation: 0.0,
       ),
-      body: Center(
-        child: Image.asset('assets/1photo.png'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('click me'),
-        backgroundColor: Colors.red,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/simoach.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height:10.0),
+            Text(
+              'MOHAMED',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height:30.0),
+            Text(
+              'CURRENT LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height:10.0),
+            Text(
+              '3/20',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height:30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width:10.0),
+                Text(
+                  'acharouaoumohamed@gmail.com',
+                  style:TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  )
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
